824a8061ea4d0a4c96704ef6a86f87a7d698358b
Gerl4ch <leonardohg2005@gmail.com>
2024-08-16 20:22:51 -0300

    Criando e inserindo registros

--- dbCantina.sql
+++ dbCantina.sql
@@ -23,8 +23,9 @@ create table tbFornecedores(
 codForn int not null auto_increment,
 nome varchar(100) not null,
 email varchar(100) not null,
-cnpj char(17) not null unique,
+cnpj char(18) not null unique,
 primary key(codForn));
+
 
 create table tbClientes(
 codCli int not null auto_increment,
@@ -64,6 +65,9 @@ primary key(codVenda),
 foreign key(codUsu)references tbUsuarios(codUsu),
 foreign key(codCli)references tbClientes(codCli),
 foreign key(codProd)references tbProdutos(codProd));
+
+insert into tbVendas(dataVenda,horaVenda,codUsu,codCli,codProd)values('2024/08/15','12:25:00',1,1,3)
+
 -- Visualizando as estruturas da tabelas 
 
 desc tbFuncionarios;
@@ -72,3 +76,53 @@ desc tbProdutos;
 desc tbFornecedores;
 desc tbClientes;
 desc tbVendas;
+
+-- inserindo registros nas tabelas
+
+insert into tbFuncionarios(nome,email,cpf,sexo,salario,nascimento,telCelular)values('Leonardo Gerlach', 'leonardohg2005@gmail.com',
+ '100.000.000-00', 'M', 1500.50, '2005/12/10', '11966066023');
+insert into tbFuncionarios(nome,email,cpf,sexo,salario,nascimento,telCelular)values('Anderson Silva', 'anderson.silva@gmail.com',
+ '100.280.000-00', 'M', 5000.00, '1998/07/05', '11945678945');
+
+
+insert into tbFornecedores(nome,email,cnpj)values('Lucius Blaque', 'LuciusBlaque@gmail.com',
+ '10.000.000/0001-00');
+insert into tbFornecedores(nome,email,cnpj)values('Elvis Presley', 'elvis123@gmail.com',
+ '10.234.000/0001-00');
+
+
+insert into tbClientes(nome,email,telCelular)values('Luisa Hartman', 'luisahart@gmail.com',
+ '11910235678');
+insert into tbClientes(nome,email,telCelular)values('Mariana Hartman', 'Marihart@gmail.com',
+ '11910235432');
+
+
+insert into tbUsuarios(nome,senha,codFunc)values('Michael Jackson','musica123',1);
+insert into tbUsuarios(nome,senha,codFunc)values('Guilherme Lima','yone321',1);
+
+
+insert into tbProdutos(descricao,quantidade,valor,validade,dataEntrada,horaEntrada,codForn)values('Computador Gamer',1,4000.00,'2026/12/01','2024/08/16','18:45:00',1);
+insert into tbProdutos(descricao,quantidade,valor,validade,dataEntrada,horaEntrada,codForn)values('Cadeira Gamer',1,2500.00,'2028/05/01','2024/08/16','20:00:00',2);
+insert into tbProdutos(descricao,quantidade,valor,validade,dataEntrada,horaEntrada,codForn)values('Fone Gamer',1,500.00,'2027/08/01','2024/08/16','21:30:00',2);
+
+
+insert into tbVendas(dataVenda,horaVenda,codUsu,codCli,codProd)values('2024/08/15','12:25:00',2,1,3);
+
+
+-- visualizando os registros das tabelas
+
+select * from tbFuncionarios;
+select * from tbFornecedores;
+select * from tbClientes;
+select * from tbUsuarios;
+select * from tbProdutos;
+select * from tbVendas;
+
+-- alterando os registro das tabelas
+
+update tbProdutos set descricao = 'Mouse Gamer', valor = 200.00 where codProd = 2;
+update tbProdutos set descricao = 'Teclado Gamer', valor = 350.00 where codProd = 1;
+
+-- visualizando depois das alterações
+
+select * from tbProdutos;
